ece69a628e111665f8da901c6bfa0797bbc63cce
Affan S <affan.s@outlook.com>
2020-10-21 00:37:24 -0400

    added threading

--- optimal_testing_covid19.jl
+++ optimal_testing_covid19.jl
@@ -1,133 +1,168 @@
-### optimal testing - covid19 project for seyed. 
-### main run function is run_sims() 
-
-using Statistics
-using Plots 
+## npt-saliva-testing
+#using JuMP, Ipopt
+using GLPK
+using DataFrames 
+using CSV 
+using LsqFit
 using Distributions
 using OffsetArrays
-using StatsBase
-using DelimitedFiles
+using Optim
+using Gnuplot
+Gnuplot.options.verbose=false
+pwd()
+println(Gnuplot.gpversion())
 
-# fixed curve for initial analysis... not used for results.
-# const rel_infect = [0, 0, 0, 0, 0, 0, 0, 0.05, 0.13, 0.58, 1, 0.87, 0.74, 0.62, 0.49, 0.37, 0.24, 0.12, 0.09, 0.05, 0.03, 0, 0, 0, 0, 0]
-# const off_rel_infect = OffsetArray(rel_infect, -10:15)
+const ll = CSV.File("data/likelihoods.csv") |> DataFrame! ; 
+const sens_csv = CSV.File("data/sensitivity.csv", header=true) |> DataFrame! ;
+const TVAL_OFFSET = -15:40 # run time for the infection.
 
-const rel_infect_lo = [0, 0, 0, 0, 0, 0, 0, 0, 0.4939, 0.8622, 0.6352, 0.4385, 0.2921, 0.1902, 0.1219, 0.0772, 0.0485, 0.0302, 0.0188, 0.0116, 0.0071, 0.0044, 0.0027, 0.0016, 0.001, 0.0006]
-const rel_infect_hi = [0, 0, 0, 0, 0, 0, 0, 0.2106, 1, 0.9992, 0.8401, 0.6135, 0.4216, 0.28, 0.182, 0.1165, 0.0737, 0.0463, 0.0288, 0.0179, 0.011, 0.0068, 0.0042, 0.0025, 0.0015, 0.0009]
-const off_rel_infect_lo = OffsetArray(rel_infect_lo, -10:15)
-const off_rel_infect_hi = OffsetArray(rel_infect_hi, -10:15)
-
-@inline @inbounds function get_a_curve()
-    rel_infect_curve = similar(rel_infect_lo) # create an empty array with length/type of rel_infect_lo
-    for i = 1:length(rel_infect_lo)
-        if (rel_infect_lo[i] + rel_infect_hi[i]) == 0 
-            rel_infect_curve[i] = 0
-        else
-            d = Uniform(rel_infect_lo[i], rel_infect_hi[i])
-            rel_infect_curve[i] = rand(d)
-        end
-    end
-    return rel_infect_curve
+## read the likelihood file from the Ashcroft paper. 
+function get_infectivity_curve(t, shape, rate, shift)
+    dist = Gamma(shape, 1/rate)    
+    pdfvals = pdf.(dist, t .+ shift) 
 end
 
-function test_get_a_curve()
-    ## this function samples 1000 get_a_curves()
-    p = plot(1:length(rel_infect_lo), rel_infect_lo, seriestype=:line, linewidth=2, seriescolor="blue")
-    plot!(p,1:length(rel_infect_lo), rel_infect_lo, seriestype=:scatter, linewidth=2, seriescolor="blue")
-    plot!(p, 1:length(rel_infect_hi), rel_infect_hi, seriestype=:line, linewidth=2, seriescolor="blue")
-    plot!(p, 1:length(rel_infect_hi), rel_infect_hi, seriestype=:scatter, linewidth=2, seriescolor="blue")
-    mycurves = [get_a_curve() for _=1:10]
-    plot!(p, mycurves)
+# we don't really use ashcroft 
+function sample_ashcroft()
+    # return shape, rate, shift from ashcroft's likelihood
+    N = nrow(ll)
+    rr = rand(1:N)  
+    shape, rate, shift = ll[rr, :].shape, ll[rr, :].rate, ll[rr, :].shift 
 end
 
-function get_lat_pre_days_fixed(exp_days)
-    lat_dist = truncated(LogNormal(log(5.2), 0.1), 4, 7) # truncated between 4 and 7
-    pre_dist = truncated(Gamma(1.058, 5/2.3), 0.8, 3)#truncated between 0.8 and 3
-    lat_days = Int.(round.(rand(lat_dist, exp_days)))
-    pre_days = Int.(round.(rand(pre_dist, exp_days)))
-    return lat_days, pre_days
+function get_ashcroft_curve(tvals)    
+    #tvals = -15:25 ## 25 since the harvard sensitivity data only has 26 points
+    randcurves = hcat([get_infectivity_curve(tvals, sample_ashcroft()...) for _=1:1000]...)
+    ymeans_ashcroft = dropdims(mean(randcurves, dims=2), dims=2) 
+    ylows_ashcroft = quantile.(eachrow(randcurves), [0.025])
+    yhighs_ashcroft = quantile.(eachrow(randcurves), [0.975])
+    return ymeans_ashcroft, ylows_ashcroft, yhighs_ashcroft
 end
 
-function get_lat_pre_days(exp_days)
-    ## exp_days = total number of days in experiment
-    lat_dist = truncated(LogNormal(log(5.2), 0.1), 2, 10) # truncated between 4 and 7
-    pre_dist = Gamma(1.058, 5/2.3) ## truncate it later. 
-    # sample exp_days of incubation 
-    lat_days = Int.(round.(rand(lat_dist, exp_days)))
-    # using the incubation days as the maximum number of presymp (substract 1 so that incubation != presymp)
-    # set up truncated distribution for latent period
-    pre_dist_trunc = truncated.(pre_dist, 0.8, (lat_days .- 1))
-    # sample the presymptomatic days (note the broadcasting since we have n distirbutions now)
-    pre_days = Int.(round.(rand.(pre_dist_trunc)))
-    return lat_days, pre_days
+function get_nature_curve(tvals)
+    #tvals = -15:25 ## 25 since the harvard sensitivity data only has 26 points
+    shrash =  20.516508, 1.592124, 12.272481    # nature.
+    ymeans_nature = get_infectivity_curve(tvals, shrash...) 
+    ymeans_offset = OffsetArray(ymeans_nature, tvals)
 end
 
-function next_tests_probs(test_type=:np, inf_type=:all, testfreq = 7, exp_days = 7)
-    #test_type=:np; inf_type=:all; testfreq = 2; exp_days = 14    
-    dayoftest = Tuple([collect(testfreq:testfreq:exp_days)...])
-    #println("days of test: $dayoftest")
-    #new code
-    lat_days, pre_days = get_lat_pre_days_fixed(exp_days) # get_lat_pre_days(exp_days)
-    rel_infect_curve = OffsetArray(get_a_curve(), -10:15)
-    #fixed code
-    # lat_days, pre_days = get_lat_pre_days_fixed(exp_days) # get_lat_pre_days(exp_days)
-    # rel_infect_curve = off_rel_infect
+function get_sensitivity_data()
+    # loads the sensitivity data from the const DataFrame
+    sens::Array{Float64, 1}  = Int64.(sens_csv.harvard[1:26])./100  # harvard data includes day 0 as symptom onset + 25 days of post symptoms
+    sens_off = OffsetArray(sens, 0:25) # offset the array for easier indexing in fitting process. has nothing to do with TVAL_OFFSET
+    return sens_off
+end
+
+# gompertz function
+function gompertz(shift_value, tvals)
+    xvals = tvals[1]:(tvals[end] + shift_value - 1)
+     _gmp = @. exp(-1*exp((-xvals)))
+    gmp = [_gmp[i+shift_value - 1] for i = 1:length(tvals)]
+    shift_gmp = OffsetArray(gmp, tvals)
+end
+
+@inline function infect_start()
+    lat_dist = truncated(LogNormal(1.434065, 0.6612), 3, 15)
+    inc_period = Int.(round.(rand(lat_dist)))
+    return inc_period .- 1 #infectiousness starts one day after incubation
+end
+
+function get_sensitivity(inf_start)
+    sens = get_sensitivity_data()
+    ngmp = gompertz(inf_start, -15:40)     ## values must match TVAL_OFFSET
+    ymeans_offset = get_nature_curve(-15:40)   
+   
+    # define function to the minimized
+    f_min(p) = sum((sens[0:25] .- p[3].*ymeans_offset[0:25].^p[1] ./ (ymeans_offset[0:25].^p[1] .+ p[2]) .* ngmp[0:25]).^2)
+    # define the modified hill function
+    @. f(x, p) = (p[3]*x^p[1]  / (x^p[1] + p[2])) *  ngmp
+    optz = optimize(f_min, [1, 0.5, 0.5]; autodiff = :forward)
+    p0 = Optim.minimizer(optz)
+    #p0 = [0.5, 0.5, 0.5]
+    #println(p0)
+    myhill = f(ymeans_offset, p0)
+    return myhill
+end
+
+function plot_sensitivity(inf_start)
+    myhill = get_sensitivity(4) # plot sensitivity curve with infectiouness starting at i days
+    println("maximum: $(maximum(myhill))")
+        
+    saliva_curves = hcat([rand(Uniform(0.70, 0.97)) * myhill / maximum(myhill) for _ = 1:1000]...)
+    saliva_mean = dropdims(mean(saliva_curves, dims=2), dims=2)
+    slows = quantile.(eachrow(saliva_curves), [0.025])
+    shighs = quantile.(eachrow(saliva_curves), [0.975])
     
-    # store results
-    days_positive = zeros(Float64, exp_days)     
-    pres_capture = zeros(Bool, exp_days)
+    @gp "set term svg enhanced standalone mouse size 800,400" ## use jupyter
+    @gp :- tit="Fitted Hill Curve" key="opaque" 
+    #@gp :- TVAL_OFFSET ymeans_offset[TVAL_OFFSET] "with lines title 'rel infectivity'"
+    @gp :- TVAL_OFFSET slows shighs "with filledcu title 'quantile' lw 2 lc rgb '#a1d99b' "
+    @gp :- TVAL_OFFSET saliva_mean "with lines title 'saliva mean' lc rgb 'black' lw 3"
+    @gp :- TVAL_OFFSET myhill[TVAL_OFFSET] "with boxes title 'npt' lc rgb 'blue'"
+    @gp :- 0:25 get_sensitivity_data()[0:25] "with points title 'sensitivity' pt 7 pointsize 0.65 lc rgb 'orange'"
+end
 
-    # go through each day of infection. 
+function next_tests_probs(test_type=:np, inf_type=:all, testfreq = 7, exp_days = 30)
+    #test_type=:np; inf_type=:all; testfreq = 5; exp_days = 20
+    dayoftest = collect(testfreq:testfreq:exp_days)
+    days_positive = zeros(Float64, exp_days)   
+
     for t = 1:exp_days
-        # get their incubation and presymptomatic period. pb < ib always.
-        ib = -lat_days[t]  
-        pb = -pre_days[t]
-        # if person is infected today (i.e. t), see what tests they are eligible for in the future.
-        # the only tests they are eligble for are the ones that are in their presymp+symp periods. 
-        possible_tests = filter(x -> x > (t + abs(ib) + pb) && x < length(ib:15) + t , dayoftest)
+        inf_start = infect_start()
+        sensitivity = get_sensitivity(inf_start)
+        maxvalue =  maximum(sensitivity)
+        if test_type == :sal 
+            sensitivity .= sensitivity ./ maxvalue # for saliva, normalize the curve.
+        end
+        
+        total_infect_days = length(sensitivity) ## get the total infectiousness period 
+        possible_tests = filter(x -> x > t && x < total_infect_days + t , dayoftest)
+
         # get the relative_infectivity index for the offset array
-        rel_idx = possible_tests .+ ib .- (t  + 1)
+        rel_idx = -15 .+ (possible_tests .- t)
         if inf_type == :pre
             rel_idx = filter(x -> x < 0, rel_idx)
         end
-        # see what happens on the day of the test and record that for t. 
+
+         # see what happens on the day of the test and record that for t. 
         tot_fails = map(rel_idx) do x 
-            infect = rel_infect_curve[x]
-            test_sens = test_type == :np ? 1.0 : rand(Uniform(0.604, 0.966))
-            trans = infect * test_sens
-            failure = 1 - trans
+            infect = sensitivity[x]
+            if test_type == :sal 
+                infect = infect * rand(Uniform(0.70, 0.97)) # saliva is done based on a normalized curve of np 
+            end
+            failure = 1 - infect
         end
         any_l = length(tot_fails)
-        prob_success = any_l > 0 ? 1 - reduce(*, tot_fails) : 0
+        prob_success = any_l > 0.0 ? 1 - reduce(*, tot_fails) : 0.0
         days_positive[t] = prob_success
     end
-    return days_positive
+    return days_positive    
 end
 
-
 function run_sims()
-    freq = (2, 4, 5, 7, 14 )
+    println("starting simulations...")
+    println("using threads: $(Threads.nthreads())")
+    N = 10
+    experiment_days = 20 
+    freq = (7, 14 )
     st = (:np, :sal)
-    tg = (:all, :pre)
+    tg = (:all, :pre)    
     for (t, g, f) in Iterators.product(st, tg, freq)
         fname = ("/data/optimal_testing/$(t)_$(f)days_$(g).csv")
         println(fname)  
-        # old code
-        N = 50000
-        experiment_days = 180      
         freq_of_test = f       
         results = zeros(Float64, experiment_days, N)
-        for i = 1:N
+        Threads.@threads for i = 1:N
             results[:, i] = next_tests_probs(t, g, freq_of_test, experiment_days)
         end
         # totals = sum(results, dims=2)./N
         # # plot(totals, seriestype=:bar)
         # # mean(totals)
-        writedlm(fname, results)
+        #writedlm(fname, results)
     end
 end
 
-run_sims()
+
 
 ### basic script testing / single scenario. 
 
